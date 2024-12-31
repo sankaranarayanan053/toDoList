0cb24124b08b3958ad93d49499c4dee008780c6a
sankaranarayanan053 <102617492+SankaraNarayanan053@users.noreply.github.com>
2024-12-31 13:03:47 +0530

    initial commit

---
+++ todolist.js
@@ -0,0 +1,50 @@
+const todoList = [{}];
+renderTodoList();
+
+function renderTodoList() {
+  let todoListHTML = '';
+
+  todoList.forEach(function(todo,index) {
+    const HTML = `
+      <div>${todo.task}</div>
+      <div>${todo.dueDate}</div>
+      <button onclick="
+        todoList.splice(${index},1);
+        renderTodoList();" 
+      class = "todo-delete">Delete</button>`;
+    todoListHTML += HTML;  
+  })
+
+  console.log(todoListHTML);
+  
+  document.querySelector('.js-todolist').innerHTML = todoListHTML;
+}
+  
+  
+  function addToDoList() {
+    const inputElement = document.querySelector('.js-todo-input');  
+    const task = inputElement.value;
+
+    const dateInputElement = document.querySelector('.js-duedate-input');
+    const dueDate = dateInputElement.value
+  
+    todoList.push({task,
+      dueDate
+    });
+    console.log(todoList);
+  
+    inputElement.value = '';
+    dateInputElement.value= '';
+    renderTodoList();
+  }
+  
+
+  
+
+
+  
+  
+  
+  
+
+ 
