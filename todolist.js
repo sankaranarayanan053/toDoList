79545e63a70179002b4974aae5876afb4d6abc49
sankaranarayanan053 <102617492+SankaraNarayanan053@users.noreply.github.com>
2025-01-03 22:41:30 +0530

    version 3

--- todolist.js
+++ todolist.js
@@ -1,50 +1,64 @@
-const todoList = [{}];
+const todoList = [];
 renderTodoList();
 
 function renderTodoList() {
   let todoListHTML = '';
 
-  todoList.forEach((todo,index) => {
+  todoList.forEach((todo, index) => {
     const HTML = `
       <div>${todo.task}</div>
       <div>${todo.dueDate}</div>
-      <button onclick="
-        todoList.splice(${index},1);
-        renderTodoList();" 
-      class = "todo-delete">Delete</button>`;
-    todoListHTML += HTML;  
+      <button class = "todo-delete js-delete-todo">Delete</button>`;
+    todoListHTML += HTML;
   })
 
   console.log(todoListHTML);
-  
+
   document.querySelector('.js-todolist').innerHTML = todoListHTML;
-}
-  
-  
-  function addToDoList() {
-    const inputElement = document.querySelector('.js-todo-input');  
-    const task = inputElement.value;
 
-    const dateInputElement = document.querySelector('.js-duedate-input');
-    const dueDate = dateInputElement.value
-  
-    todoList.push({task,
-      dueDate
+
+  document.querySelector('.js-add-input')
+    .addEventListener('click', () => {
+      addToDoList();
     });
-    console.log(todoList);
-  
-    inputElement.value = '';
-    dateInputElement.value= '';
-    renderTodoList();
-  }
-  
 
-  
+  document.querySelectorAll('.js-delete-todo')
+    .forEach((deleteButton, index) => {
+      deleteButton.addEventListener('click', () => {
+        todoList.splice(index, 1);
+        renderTodoList();
+
+      })
+    })
+}
 
 
-  
-  
-  
-  
 
- 
+function addToDoList() {
+  const inputElement = document.querySelector('.js-todo-input');
+  const task = inputElement.value;
+
+  const dateInputElement = document.querySelector('.js-duedate-input');
+  const dueDate = dateInputElement.value
+
+  todoList.push({
+    task,
+    dueDate
+  });
+  console.log(todoList);
+
+  inputElement.value = '';
+  dateInputElement.value = '';
+  renderTodoList();
+}
+
+
+
+
+
+
+
+
+
+
+
